ef29fab6cc79cec2513f634091fa2dfe00166276
Adarsh Kumar <120160355+jinx-vi-0@users.noreply.github.com>
2023-08-26 23:17:38 +0530

    Time: 2 ms (99.67%), Space: 44.1 MB (31.68%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -2,14 +2,14 @@ class Solution {
     public List<Integer> intersection(int[][] nums) {
         List<Integer> list = new ArrayList<>();
         int[] count  = new int[1001];
-        for (int[] num : nums) {
-            for (int i : num) {
+        for (int[] num : nums)
+            for (int i : num)
                 count[i]++;
-                if (count[i] == nums.length)
-                    list.add(i);            
-            }
-        }
-        Collections.sort(list);
+        
+        for(int i=0; i<count.length; i++)
+           if(count[i] == nums.length)
+               list.add(i);
+        
         return list;
     }
 }
