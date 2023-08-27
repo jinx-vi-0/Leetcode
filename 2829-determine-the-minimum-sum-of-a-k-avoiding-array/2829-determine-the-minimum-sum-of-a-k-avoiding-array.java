7f00a385450328e1ac68db7b21483284a77e1b74
Adarsh Kumar <120160355+jinx-vi-0@users.noreply.github.com>
2023-08-27 20:42:45 +0530

    Time: 2 ms (88.02%), Space: 39.6 MB (99.93%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
@@ -1,13 +1,17 @@
 class Solution {
     public int minimumSum(int n, int k) {
         int sum = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int i=1; set.size()<n; i++) {
-            if(!set.contains(k-i)) {
-                set.add(i);
-                sum += i;
-            }
+        int temp = k/2;
+        for(int i=1; i<=temp; i++) {
+            sum += i;
+            n--;
+            if(n == 0) return sum;
         }
+        temp = k;
+        while(n-->0) {
+            sum += temp;
+            temp++;
+        }        
         return sum;
     }
 }
