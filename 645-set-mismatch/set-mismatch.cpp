2aae10b6a17bceb7d9250b21dd733ac72a8382a7
Adarsh <120160355+jinx-vi-0@users.noreply.github.com>
2024-01-22 23:14:11 +0530

    Time: 15 ms (97.34%) | Memory: 23.7 MB (36.81%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans(2);
+        for(int i : nums) {
+            if(nums[abs(i) - 1] < 0)
+                ans[0] = abs(i);
+            else
+                nums[abs(i) - 1] *= -1;
+        }
+
+        for(int i=0; i<nums.size(); i++)
+            if(nums[i] > 0) ans[1] = i+1;
+        
+        return ans;
+    }
+};
